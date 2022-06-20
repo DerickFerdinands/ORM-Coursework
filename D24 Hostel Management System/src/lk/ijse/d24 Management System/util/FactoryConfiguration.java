1d9472544d31915d6f90f7d642619f7117fb8e87
DerickFerdinands <derickandrewferdinands@gmail.com>
2022-06-20 14:48:57 +0530

    Propert File Configuration Added

---
+++ D24 Hostel Management System/src/lk/ijse/d24 Management System/util/FactoryConfiguration.java
@@ -0,0 +1,25 @@
+package util;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class FactoryConfiguration {
+    private static FactoryConfiguration factoryConfiguration;
+    private  SessionFactory sessionFactory;
+
+    private FactoryConfiguration() {
+
+        Configuration config = new Configuration();
+        config.addAnnotatedClass(Student.class).addAnnotatedClass(User.class);
+        sessionFactory = config.buildSessionFactory();
+    }
+
+    public static FactoryConfiguration getInstance() {
+        return factoryConfiguration == null ? factoryConfiguration = new FactoryConfiguration() : factoryConfiguration;
+    }
+
+    public Session getSession() {
+        return sessionFactory.openSession();
+    }
+}
