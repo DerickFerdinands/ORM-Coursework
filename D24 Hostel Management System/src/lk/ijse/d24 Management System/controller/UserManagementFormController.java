162f06196241ba1a5f64ecef609355db2bca40f8
DerickFerdinands <derickandrewferdinands@gmail.com>
2022-06-22 08:02:07 +0530

    Second Level Caching Configured

--- D24 Hostel Management System/src/lk/ijse/d24 Management System/controller/UserManagementFormController.java
+++ D24 Hostel Management System/src/lk/ijse/d24 Management System/controller/UserManagementFormController.java
@@ -1,8 +1,24 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 
 public class UserManagementFormController {
+    public JFXTextField txtNic;
+    public JFXTextField txtConfirmPassword;
+    public JFXTextField txtPassword;
+    public JFXTextField txtName;
+    public JFXTextField txtUserName;
+    public JFXButton btnAddUser;
+    public JFXButton btnCancel;
+
     public void addUserOnAction(ActionEvent actionEvent) {
+        if(btnAddUser.getText().equals("Add User")){
+
+        }
+    }
+
+    public void clearFormOnAction(ActionEvent actionEvent) {
     }
 }
